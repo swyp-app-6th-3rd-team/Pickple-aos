6676b1fa15c8b29590b2acf9db39f3cbbd5c65ef
deoya <deoya@naver.com>
2026-08-30 00:14:26 +0900

    [feat/#9] 도메인 뼈대 설정

---
+++ domain/src/main/java/com/swyp/domain/common/result/ResultWrapper.kt
@@ -0,0 +1,6 @@
+package com.swyp.domain.common
+
+sealed class ResultWrapper<out T, out E> {
+    data class Success<out T>(val value: T) : ResultWrapper<T, Nothing>()
+    data class Failure<out E>(val error: E) : ResultWrapper<Nothing, E>()
+}
