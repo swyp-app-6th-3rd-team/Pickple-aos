8deb80c85730cd949f2ed7e40cb99029e7a02ddc
deoya <deoya@naver.com>
2026-08-31 16:38:27 +0900

    [feat/#9] apple 로그인 구현 (임시)

--- data/src/main/java/com/swyp/data/auth/repository/impl/AuthRepositoryImpl.kt
+++ data/src/main/java/com/swyp/data/auth/repository/impl/AuthRepositoryImpl.kt
@@ -2,54 +2,53 @@ package com.swyp.data.auth.repository.impl
 
 import com.swyp.data.auth.di.qualifier.AppleAuth
 import com.swyp.data.auth.di.qualifier.KakaoAuth
+import com.swyp.data.auth.source.RemoteAuthDataSource
 import com.swyp.data.auth.source.SocialAuthDataSource
 import com.swyp.domain.auth.model.AuthError
 import com.swyp.domain.auth.model.AuthProvider
 import com.swyp.domain.auth.model.AuthResult
 import com.swyp.domain.auth.repository.AuthRepository
-import com.swyp.domain.common.ResultWrapper
-import timber.log.Timber
+import com.swyp.domain.common.result.ResultWrapper
 import javax.inject.Inject
 
 class AuthRepositoryImpl @Inject constructor(
     @KakaoAuth private val kakaoAuthDataSource: SocialAuthDataSource,
     @AppleAuth private val appleAuthDataSource: SocialAuthDataSource,
+    private val remoteAuthDataSource: RemoteAuthDataSource,
 ) : AuthRepository {
 
     override suspend fun loginWithSocial(
         provider: AuthProvider,
     ): ResultWrapper<AuthResult, AuthError> {
 
-        val result = when(provider){
-            AuthProvider.KAKAO -> kakaoAuthDataSource.login()
-            AuthProvider.APPLE -> appleAuthDataSource.login()
+
+        val socialToken = when (provider) {
+            AuthProvider.KAKAO ->
+                kakaoAuthDataSource.login()
+
+            AuthProvider.APPLE ->
+                appleAuthDataSource.login()
         }
-        return result.fold(
-            // 테스트를 위해 신규 유저(isNewUser = true) 상태를 반환하도록 Mocking
-            onSuccess = {
-                //Todo: token 저장
-                ResultWrapper.Success(
-                    AuthResult(
-                        accessToken = "dummy_access_token",
-                        refreshToken = "dummy_refresh_token",
-                        isNewUser = true
-                    )
-                )
-            },
-            onFailure = { error  ->
-                Timber.d("로그인 실패 ${error.message}")
-                ResultWrapper.Failure(AuthError.Unknown(error.message) )
-            }
+
+        val providerToken = socialToken.getOrElse { exception ->
+            return ResultWrapper.Failure(
+                exception.toAuthError()
+            )
+        }
+
+        return remoteAuthDataSource.login(
+            provider = provider,
+            providerToken = providerToken,
         )
     }
 
     override suspend fun loginAsGuest(): ResultWrapper<AuthResult, AuthError> {
-        return ResultWrapper.Success(
-            AuthResult(
-                accessToken = "guest_access_token",
-                refreshToken = "guest_refresh_token",
-                isNewUser = false
-            )
-        )
+        return remoteAuthDataSource.loginAsGuest()
+    }
+}
+
+private fun Throwable.toAuthError(): AuthError {
+    return when (this) {
+        else -> AuthError.Unknown(message)
     }
 }
--- data/src/main/java/com/swyp/data/auth/source/impl/AppleAuthDataSourceImpl.kt
+++ data/src/main/java/com/swyp/data/auth/source/impl/AppleAuthDataSourceImpl.kt
@@ -1,9 +1,30 @@
 package com.swyp.data.auth.source.impl
 
 import com.swyp.data.auth.source.SocialAuthDataSource
+import timber.log.Timber
+import javax.inject.Inject
 
-class AppleAuthDataSourceImpl : SocialAuthDataSource {
+class AppleAuthDataSourceImpl @Inject constructor() : SocialAuthDataSource {
+
+    // 테스트를 위한 임시 값
+    private companion object {
+        const val APPLE_AUTH_URL = "https://appleid.apple.com/auth/authorize"
+        const val CLIENT_ID = "com.swyp.pickple.login" // Service ID
+        const val REDIRECT_URI = "https://pickple.com/api/auth/apple/callback"
+    }
+
     override suspend fun login(): Result<String> {
-        TODO("Not yet implemented")
+        Timber.d("애플 로그인 url : ${buildAppleAuthUrl()}")
+
+        return Result.success("dummy_apple_id_token")
+    }
+
+    private fun buildAppleAuthUrl(): String {
+        return APPLE_AUTH_URL +
+                "?client_id=$CLIENT_ID" +
+                "&redirect_uri=$REDIRECT_URI" +
+                "&response_type=code id_token" +
+                "&scope=name email" +
+                "&response_mode=form_post"
     }
 }
